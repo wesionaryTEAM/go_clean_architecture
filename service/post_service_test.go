646ffc16a85adbd188168c25ac2a2b19aca3d545
Cyantosh <shresthsantosh799@gmail.com>
2020-06-24 17:25:12 +0545

    Test cases for validate method of post service done

--- service/post_service_test.go
+++ service/post_service_test.go
@@ -1,8 +1,8 @@
 package service
 
 import (
-	"testing"
 	"prototype2/entity"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -12,7 +12,7 @@ type MockRepository struct {
 	mock.Mock
 }
 
-func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error){
+func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
 	args := mock.Called()
 	result := args.Get(0)
 	return result.(*entity.Post), args.Error(1)
@@ -24,11 +24,39 @@ func (mock *MockRepository) FindAll() ([]entity.Post, error) {
 	return result.([]entity.Post), args.Error(1)
 }
 
+func TestValidateEmptyPost(t *testing.T) {
+	testService := NewPostService(nil)
+
+	err := testService.Validate(nil)
+
+	assert.NotNil(t, err)
+
+	assert.Equal(t, "The post is empty", err.Error())
+}
+
+func TestValidateEmptyPostField(t *testing.T) {
+	testService := NewPostService(nil)
+
+	scenarios := []struct {
+		post   entity.Post
+		expect string
+	}{
+		{post: entity.Post{ID: 1, Title: "", Text: "Ball"}, expect: "The post title is empty"},
+		{post: entity.Post{ID: 1, Title: "Apple", Text: ""}, expect: "The post text is empty"},
+	}
+
+	for _, s := range scenarios {
+		err := testService.Validate(&s.post)
+		assert.NotNil(t, err)
+		assert.Equal(t, s.expect, err.Error())
+	}
+}
+
 func TestFindAll(t *testing.T) {
 	mockRepo := new(MockRepository)
 
 	var identifier int64 = 1
-	
+
 	post := entity.Post{ID: identifier, Title: "Apple", Text: "Ball"}
 
 	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
@@ -44,8 +72,7 @@ func TestFindAll(t *testing.T) {
 	assert.Equal(t, "Ball", result[0].Text)
 }
 
-
-func TestCreate(t *testing.T){
+func TestCreate(t *testing.T) {
 	mockRepo := new(MockRepository)
 	post := entity.Post{Title: "Test Title", Text: "Test Text"}
 
