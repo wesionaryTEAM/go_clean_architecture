6849e566e0922a407ac55f7d0b9391d1e0b11b4d
Santosh Shrestha <50645720+Cyantosh0@users.noreply.github.com>
2020-06-25 09:16:48 +0545

    Test cases for validate method of post service done (#5)
    Co-authored-by: Cyantosh <shresthsantosh799@gmail.com>

--- service/post_service.go
+++ service/post_service.go
@@ -1,42 +1,46 @@
 package service
 
 import (
+	"errors"
 	"math/rand"
 	"prototype2/entity"
 	repository "prototype2/repository"
-	"errors"
 )
 
 type PostService interface {
 	Validate(post *entity.Post) error
-	Create(post  *entity.Post) (*entity.Post, error)
+	Create(post *entity.Post) (*entity.Post, error)
 	FindAll() ([]entity.Post, error)
 }
 
-type service struct {}
+type service struct{}
 
 var (
 	repo repository.PostRepository //= repository.NewFirestoreRepository()
 )
 
-func NewPostService(repository repository.PostRepository) PostService{	
+func NewPostService(repository repository.PostRepository) PostService {
 	repo = repository
 	return &service{}
 }
 
 func (*service) Validate(post *entity.Post) error {
-	if(post == nil){
+	if post == nil {
 		err := errors.New("The post is empty")
 		return err
 	}
-	if(post.Title == ""){
+	if post.Title == "" {
 		err := errors.New("The post title is empty")
+		return err
+	}
+	if post.Text == "" {
+		err := errors.New("The post text is empty")
 		return err
 	}
 	return nil
 }
 
-func (*service) Create(post  *entity.Post) (*entity.Post, error) {
+func (*service) Create(post *entity.Post) (*entity.Post, error) {
 	post.ID = rand.Int63()
 	return repo.Save(post)
 }
