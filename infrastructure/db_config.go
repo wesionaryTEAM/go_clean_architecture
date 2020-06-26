943fb69548bcd473001174dd9729669f56db5d5d
Suz'l Shrestha <sujal.kokh@gmail.com>
2020-06-26 15:03:54 +0545

    removed print statement in db_config.go

--- infrastructure/db_config.go
+++ infrastructure/db_config.go
@@ -18,7 +18,6 @@ func SetupModels() *gorm.DB {
 	DBNAME := get("DB_NAME")
 
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
-	fmt.Println(URL)
 	db, err := gorm.Open("mysql", URL)
 
 	if err != nil {
