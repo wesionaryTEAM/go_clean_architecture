990d7afe2a411dfd3dd3db472cb6661320af1a3d
Suz'l Shrestha <sujal.kokh@gmail.com>
2020-07-03 12:08:05 +0545

    sentry setup completed for personal DSN sujal.kokh

---
+++ utils/sentry_config.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func SetupSentry() {
+	get := GetEnvWithKey
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: get("SENTRY_DSN"),
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+}
