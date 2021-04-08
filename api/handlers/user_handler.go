acb22bf2b08c97e166ee21597b4fbdb199f3f912
Pragyan Aryal <pragyanaryal4@outlook.com>
2021-04-08 10:26:03 +0545

    SYS: TESTING GIT-PRE_HOOK

--- api/handlers/user_handler.go
+++ api/handlers/user_handler.go
@@ -1,35 +1,37 @@
 package handlers
 
 import (
-	"github.com/gin-gonic/gin"
 	"prototype2/interfaces"
 	"prototype2/lib"
+
+	"github.com/gin-gonic/gin"
 )
 
-type UserController struct {logger lib.Logger
+type UserController struct {
+	logger      lib.Logger
 	userService interfaces.UserServiceInterface
 }
 
-func NewUserController(logger lib.Logger, serviceInterface interfaces.UserServiceInterface) UserController{
+func NewUserController(logger lib.Logger, serviceInterface interfaces.UserServiceInterface) UserController {
 	return UserController{logger: logger, userService: serviceInterface}
 }
 
-func (u *UserController) CreateUser(c * gin.Context) {
+func (u *UserController) CreateUser(c *gin.Context) {
 
 }
 
-func (u *UserController) GetUsers(c * gin.Context) {
+func (u *UserController) GetUsers(c *gin.Context) {
 
 }
 
-func (u *UserController) GetSingleUser(c * gin.Context) {
+func (u *UserController) GetSingleUser(c *gin.Context) {
 
 }
 
-func (u *UserController) PatchUsers(c * gin.Context) {
+func (u *UserController) PatchUsers(c *gin.Context) {
 
 }
 
-func (u *UserController) DeleteUser(c * gin.Context) {
+func (u *UserController) DeleteUser(c *gin.Context) {
 
 }
