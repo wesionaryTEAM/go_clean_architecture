3b51b9a29ab0572d72791b7a56a784a2f879b585
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-20 10:49:22 +0545

    feat: update user model

--- models/user.go
+++ models/user.go
@@ -1,17 +1,23 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	"time"
+)
 
-// User represents the user for this application
-//
-// User is the database model for user.
-//
-// swagger:model
+// User model
 type User struct {
-	ID uuid.UUID `json:"id"`
+	ID           uint           `json:"id"`
+	Name         string         `json:"name"`
+	Email        *string        `json:"email"`
+	Age          uint8          `json:"age"`
+	Birthday     *time.Time     `json:"time"`
+	MemberNumber sql.NullString `json:"member_number"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
 }
 
+// TableName gives table name of model
 func (u User) TableName() string {
-	// Should return the name of table used in database. This is for the purpose of gorm
 	return "users"
 }
