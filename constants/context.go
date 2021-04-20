f00a00b027fffcfe5d9635569b460052e890ca7c
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-20 10:49:17 +0545

    feat: added context

---
+++ constants/context.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	// DBTransaction is database transaction handle set at router context
+	DBTransaction = "db_trx"
+
+	// Claims -> authentication claims
+	Claims = "Claims"
+
+	// UID -> authenticated user's id
+	UID = "UID"
+)
