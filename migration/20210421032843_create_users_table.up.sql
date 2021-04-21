7667ff6fb6870b24ce6f4e668e12315ee378c415
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-21 09:16:49 +0545

    feat: added sample migration files

---
+++ migration/20210421032843_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,  
+  `email` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(20) NOT NULL,
+  `age` int(10) UNSIGNED,
+  `birthday` DATETIME,
+  `member_number` VARCHAR(100),
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT email_unique UNIQUE(email)
+)ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
