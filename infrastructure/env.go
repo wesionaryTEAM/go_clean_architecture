37cc23419618631441b1f8deaec43d1cc50ae56e
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-30 12:48:51 +0545

    fix: env map structure mapping

--- infrastructure/env.go
+++ infrastructure/env.go
@@ -10,7 +10,7 @@ type Env struct {
 	ServerPort        string `mapstructure:"SERVER_PORT"`
 	Environment       string `mapstructure:"ENVIRONMENT"`
 	DBUsername        string `mapstructure:"DB_USER"`
-	DBPassword        string `mapstructure:"DB_PASSWORD"`
+	DBPassword        string `mapstructure:"DB_PASS"`
 	DBHost            string `mapstructure:"DB_HOST"`
 	DBPort            string `mapstructure:"DB_PORT"`
 	DBName            string `mapstructure:"DB_NAME"`
