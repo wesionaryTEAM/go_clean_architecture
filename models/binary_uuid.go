b04b56482c8a19f9f662742bb23e9b79234b63d3
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-05-22 14:28:30 +0545

    feat: added binary uuid

---
+++ models/binary_uuid.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+// BinaryUUID -> binary uuid wrapper over uuid.UUID
+type BinaryUUID uuid.UUID
+
+// ParseUUID -> parses string uuid to binary uuid
+func ParseUUID(id string) BinaryUUID {
+	return BinaryUUID(uuid.MustParse(id))
+}
+
+func (b BinaryUUID) String() string {
+	return uuid.UUID(b).String()
+}
+
+// MarshalJSON -> convert to json string
+func (b BinaryUUID) MarshalJSON() ([]byte, error) {
+	s := uuid.UUID(b)
+	str := "\"" + s.String() + "\""
+	return []byte(str), nil
+}
+
+// UnmarshalJSON -> convert from json string
+func (b *BinaryUUID) UnmarshalJSON(by []byte) error {
+	s, err := uuid.ParseBytes(by)
+	*b = BinaryUUID(s)
+	return err
+}
+
+// GormDataType -> sql data type for gorm
+func (BinaryUUID) GormDataType() string {
+	return "binary(16)"
+}
+
+// Scan -> scan value into BinaryUUID
+func (b *BinaryUUID) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	data, err := uuid.FromBytes(bytes)
+	*b = BinaryUUID(data)
+	return err
+}
+
+// Value -> return BinaryUUID to []bytes binary(16)
+func (b BinaryUUID) Value() (driver.Value, error) {
+	return uuid.UUID(b).MarshalBinary()
+}
--- models/user.go
+++ models/user.go
@@ -3,21 +3,30 @@ package models
 import (
 	"database/sql"
 	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 // User model
 type User struct {
-	ID           uint           `json:"id"`
-	Name         string         `json:"name"`
-	Email        *string        `json:"email"`
-	Age          uint8          `json:"age"`
+	Base
+	Name         string         `json:"name" form:"name"`
+	Age          int            `json:"age" form:"age"`
 	Birthday     *time.Time     `json:"time"`
 	MemberNumber sql.NullString `json:"member_number"`
-	CreatedAt    time.Time      `json:"created_at"`
-	UpdatedAt    time.Time      `json:"updated_at"`
+	CreatedAt    time.Time      `json:"created_at" form:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at" form:"updated_at"`
 }
 
 // TableName gives table name of model
 func (u User) TableName() string {
 	return "users"
+}
+
+// BeforeCreate run this before creating user
+func (t *User) BeforeCreate(tx *gorm.DB) error {
+	id, err := uuid.NewRandom()
+	t.ID = BinaryUUID(id)
+	return err
 }
