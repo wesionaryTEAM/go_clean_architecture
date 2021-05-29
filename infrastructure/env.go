7db6deaeda8b931c6f0ca7fc396fb45f5b289159
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-05-29 12:50:40 +0545

    chore: added max multipart memory env variable

--- infrastructure/env.go
+++ infrastructure/env.go
@@ -7,22 +7,25 @@ import (
 )
 
 type Env struct {
-	ServerPort        string `mapstructure:"SERVER_PORT"`
-	Environment       string `mapstructure:"ENVIRONMENT"`
-	DBUsername        string `mapstructure:"DB_USER"`
-	DBPassword        string `mapstructure:"DB_PASS"`
-	DBHost            string `mapstructure:"DB_HOST"`
-	DBPort            string `mapstructure:"DB_PORT"`
-	DBName            string `mapstructure:"DB_NAME"`
-	SentryDSN         string `mapstructure:"SENTRY_DSN"`
-	StorageBucketName string `mapstructure:"STORAGE_BUCKET_NAME"`
-	MailClientID      string `mapstructure:"MAIL_CLIENT_ID"`
-	MailClientSecret  string `mapstructure:"MAIL_CLIENT_SECRET"`
-	MailTokenType     string `mapstructure:"MAIL_TOKEN_TYPE"`
+	ServerPort         string `mapstructure:"SERVER_PORT"`
+	Environment        string `mapstructure:"ENVIRONMENT"`
+	DBUsername         string `mapstructure:"DB_USER"`
+	DBPassword         string `mapstructure:"DB_PASS"`
+	DBHost             string `mapstructure:"DB_HOST"`
+	DBPort             string `mapstructure:"DB_PORT"`
+	DBName             string `mapstructure:"DB_NAME"`
+	MaxMultipartMemory int64  `mapstructure:"MAX_MULTIPART_MEMORY"`
+	SentryDSN          string `mapstructure:"SENTRY_DSN"`
+	StorageBucketName  string `mapstructure:"STORAGE_BUCKET_NAME"`
+	MailClientID       string `mapstructure:"MAIL_CLIENT_ID"`
+	MailClientSecret   string `mapstructure:"MAIL_CLIENT_SECRET"`
+	MailTokenType      string `mapstructure:"MAIL_TOKEN_TYPE"`
 }
 
 func NewEnv() Env {
-	env := Env{}
+	env := Env{
+		MaxMultipartMemory: 10 << 20, // 10 MB
+	}
 	viper.SetConfigFile(".env")
 
 	err := viper.ReadInConfig()
