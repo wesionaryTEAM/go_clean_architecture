8aaebeba2e5f3338aab6f2559674bd9f8f1a3183
Cyantosh0 <shresthasantosh799@gmail.com>
2021-12-28 14:00:55 +0545

    Add log in pagination middleware

--- api/middlewares/pagination_middleware.go
+++ api/middlewares/pagination_middleware.go
@@ -2,20 +2,24 @@ package middlewares
 
 import (
 	"clean-architecture/constants"
+	"clean-architecture/lib"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type PaginationMiddleware struct {
+	logger lib.Logger
 }
 
-func NewPaginationMiddleware() PaginationMiddleware {
-	return PaginationMiddleware{}
+func NewPaginationMiddleware(logger lib.Logger) PaginationMiddleware {
+	return PaginationMiddleware{logger: logger}
 }
 
 func (p PaginationMiddleware) Handle() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		p.logger.Info("setting up pagination middleware")
+
 		per_page, err := strconv.ParseInt(c.Query("per_page"), 10, 0)
 		if err != nil {
 			per_page = 10
