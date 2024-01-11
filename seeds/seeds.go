b59aa87951a0cdb056cdc7d06aa431bb9f0acda1
Sudarshan Pathak <sushan.pth@gmail.com>
2024-01-11 16:11:04 +0545

    add seed:run cli command

--- seeds/seeds.go
+++ seeds/seeds.go
@@ -10,18 +10,13 @@ var Module = fx.Options(
 
 // Seed db seed
 type Seed interface {
+	// name is used to identify the seed
+	Name() string
 	Setup()
 }
 
 // Seeds listing of seeds
 type Seeds []Seed
-
-// Run run the seed data
-func (s Seeds) Setup() {
-	for _, seed := range s {
-		seed.Setup()
-	}
-}
 
 // NewSeeds creates new seeds
 func NewSeeds(adminSeed AdminSeed) Seeds {
