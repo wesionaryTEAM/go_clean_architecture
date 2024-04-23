6e8a410ed31bcd3e2a8d4f99cd4274b437ab7bb7
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-04-23 13:21:14 +0545

    feat: handle validation error

--- domain/models/user.go
+++ domain/models/user.go
@@ -13,11 +13,11 @@ import (
 // User model
 type User struct {
 	types.ModelBase
-	Name         string          `json:"name" form:"name"`
-	Email        string          `json:"email" form:"email"`
-	Age          int             `json:"age" form:"age"`
-	Birthday     *time.Time      `json:"time"`
-	MemberNumber sql.NullString  `json:"member_number"`
+	Name         string          `json:"name" form:"name" binding:"required"`
+	Email        string          `json:"email" form:"email "binding:"required,email"`
+	Age          int             `json:"age" form:"age" binding:"required"`
+	Birthday     *time.Time      `json:"time" form:"time" binding:"required" time_format:"2006-01-02"`
+	MemberNumber sql.NullString  `json:"member_number" binding:"required"`
 	ProfilePic   utils.SignedURL `json:"profile_pic"`
 	CreatedAt    time.Time       `json:"created_at" form:"created_at"`
 	UpdatedAt    time.Time       `json:"updated_at" form:"updated_at"`
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/getsentry/sentry-go v0.23.0
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/validator/v10 v10.15.0
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
@@ -45,7 +46,6 @@ require (
 	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.15.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
